a224bf3c99a3c54c7fba0bbf2bf30150d5ab2245
sinlyon <2532536644@qq.com>
2019-03-21 17:21:13 +0800

    Initial commit

---
+++ HelloWorld/view.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding:utf-8
+# author: sinlyon date 2019/3/21 0021
+# from django.http import HttpResponse
+import time
+
+from django.shortcuts import render
+
+
+def hello(request):
+    context = {}
+    context['hello'] = 'Hello World. '
+    context['str1'] = "abcdef"
+    context['users'] = ["sinlyon", "amanbo"]
+    context["date"] = time.time()
+    return render(request, 'hello.html', context)
+    # return ren
